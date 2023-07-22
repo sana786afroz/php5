c1fab139be68341dc8bcbc9734a31aa73372ac08
Sana Afroz <119676298+sana786afroz@users.noreply.github.com>
2023-07-22 20:16:08 +0530

    Add files via upload

---
+++ basic1.php
@@ -0,0 +1,25 @@
+<?php 
+define('pi',5.55);
+echo "Datatypes in php <br>"; 
+
+echo "string <br>";
+echo "integer <br>";
+echo "float <br>";
+echo "boolean <br>";
+echo "array<br>";
+echo "object <br>";
+$var="this is string";
+
+echo var_dump($var);
+echo "<br>";
+$var1=55;
+echo var_dump($var1);
+$var1= 55.4;
+echo "<br>";
+echo var_dump($var1);
+$var1= true;
+echo "<br>";
+echo var_dump($var1);
+echo "<br>";
+echo pi;
+?>
